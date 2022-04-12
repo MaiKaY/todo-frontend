4b0a21aae5ac56a8b6302c57e3a9229e23cbc3df
Maik Schmidt <maik.schmidt.hl@gmail.com>
2022-04-12 20:09:10 +0200

    chore: adjust endpoint

--- src/graphql/types.ts
+++ src/graphql/types.ts
@@ -8,7 +8,7 @@ export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Mayb
 
 function fetcher<TData, TVariables>(query: string, variables?: TVariables) {
     return async (): Promise<TData> => {
-        const res = await fetch('https://staging-api.todo.maikay.de/graphql', {
+        const res = await fetch('https://api.todo.maikay.de/graphql', {
             method: 'POST',
             ...{ headers: { 'Content-Type': 'application/json' } },
             body: JSON.stringify({ query, variables })
